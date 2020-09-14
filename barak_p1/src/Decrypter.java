88bf9a153635385b70815deae79a731910ef6986
Your Name <you@example.com>
2020-09-14 13:55:44 -0400

    First assignment finished

---
+++ barak_p1/src/Decrypter.java
@@ -0,0 +1,26 @@
+public class Decrypter {
+
+    public Decrypter(){
+
+    }
+    String decrypt(String num){
+        int n = num.length();
+        int[] vals = new int[n];
+        for(int i =0;i<n;i++){
+            vals[i] = (num.charAt(i)-'0'+3)%10;
+        }
+        boolean[] v = new boolean[n];
+        for(int i = 0;i<n-2;i++){
+            if(v[i])continue;
+            v[i+2] = true;
+            int temp = vals[i];
+            vals[i] = vals[i+2];
+            vals[i+2] = temp;
+        }
+        String res = "";
+        for(int i = 0;i<n;i++){
+            res+=vals[i];
+        }
+        return res;
+    }
+}
