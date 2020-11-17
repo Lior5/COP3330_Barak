2bbc611beb85bd1f291f8a776af9ba5b5a7f561c
Your Name <you@example.com>
2020-11-16 22:42:43 -0500

    Assignment Completed!

---
+++ barak_p4/src/TaskList.java
@@ -0,0 +1,8 @@
+public class TaskItem {
+    private String title, description, dueDate;
+    public TaskItem(String title, String description,String dueDate){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+}
